4ca3db048356dcd7ede3147240e611fe5c3038d6
loveline-so <lovelinesokou@mail.com>
2020-05-04 17:46:26 +0100

    interfaces de Bible eLearning

---
+++ app/src/main/java/com/loveline/myelearning/Appercu.java
@@ -0,0 +1,14 @@
+package com.loveline.myelearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appercu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appercu);
+    }
+}
